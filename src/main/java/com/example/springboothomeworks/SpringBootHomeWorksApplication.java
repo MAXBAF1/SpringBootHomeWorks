cebf098b0947f950d7b72622b04dd00cc22946d6
Мax <maxlepinskih@gmail.com>
2024-02-24 09:15:58 +0500

    Initial commit

---
+++ src/main/java/com/example/springboothomeworks/SpringBootHomeWorksApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboothomeworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHomeWorksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHomeWorksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboothomeworks/SpringBootHomeWorksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboothomeworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHomeWorksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
